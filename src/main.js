44539f452033c6b0dfb05f2f08e202710bba6918
AaronXue <her8153101@yahoo.com.tw>
2020-05-30 19:14:31 +0800

    Connected firebase

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import firebase from "firebase";
+import config from "./firebaseConfig.js";
 
 Vue.config.productionTip = false;
+firebase.initializeApp(config);
+firebase.analytics();
 
 new Vue({
   router,
