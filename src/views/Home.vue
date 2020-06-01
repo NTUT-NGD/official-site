ec7eb92ca918aef9e8d5bde21aef4a1533b2e703
AaronXue <her8153101@yahoo.com.tw>
2020-06-01 14:54:03 +0800

    Add sample vue component of form and API post

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <v-btn @click="create">Create</v-btn>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
-  components: {}
+  components: {},
+  methods: {
+    create() {}
+  }
 };
 </script>
