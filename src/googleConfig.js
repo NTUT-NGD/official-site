40745d552ed4fa1d4ac4696f8f1782b89c9f53b4
AaronXue <her8153101@yahoo.com.tw>
2020-06-07 19:47:15 +0800

    Add calendar.vue without connecting google API

--- src/googleConfig.js
+++ src/googleConfig.js
@@ -2,7 +2,8 @@ const googleConfig = {
   apiKey: "AIzaSyBq1cn7X_MhHMzD-Su_zAoeEzri6plqluE",
   clientId:
     "857901293436-e869ssrl110gqhd5kbi33p759slcpisn.apps.googleusercontent.com",
-  SCOPES: ["https://www.googleapis.com/auth/calendar"],
+  scopes:
+    "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/admin.directory.resource.calendar",
   DISCOVERY_DOCS: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"]
 };
 
