c56df170867f9f24097474fc1dfb3485361b7c75
AaronXue <her8153101@yahoo.com.tw>
2020-06-16 03:49:32 +0800

    Setup about game

--- src/components/about/game.vue
+++ src/components/about/game.vue
@@ -4,7 +4,7 @@
       ref="board"
       width="640"
       height="480"
-      style="border: 2px solid black;"
+      style="border: 1px solid black;"
     ></canvas>
     <br />
     <v-row>
@@ -30,7 +30,7 @@
 </template>
 
 <script>
-import io from "socket.io-client";
+// import io from "socket.io-client";
 export default {
   name: "about",
   components: {},
@@ -41,30 +41,61 @@ export default {
       socket: {},
       context: {},
       position: {
-        x: 0,
-        y: 0
-      }
+        x: 200,
+        y: 200,
+        w: 20,
+        h: 20,
+        originX: 200,
+        originY: 200
+      },
+      builds: [
+        { x: 0, y: 0, w: 50, h: 50, infos: "北科遊戲設計社" },
+        { x: 100, y: 100, w: 50, h: 50, infos: "成立時間：108-2, 03/15" }
+      ]
     };
   },
   created() {
-    this.socket = io("http://localhost:3000");
+    // this.socket = io("http://localhost:3000");
   },
   methods: {
     move(direction) {
-      this.socket.emit("move", direction);
+      let movement = 20;
+      switch (direction) {
+        case "right":
+          this.position.x += movement;
+          break;
+        case "left":
+          this.position.x -= movement;
+          break;
+        case "up":
+          this.position.y -= movement;
+          break;
+        case "down":
+          this.position.y += movement;
+          break;
+      }
+      this.collidingDetect();
+      this.doDraw();
     },
     reset() {
-      this.socket.emit("reset");
-    }
-  },
-  mounted() {
-    this.$store.commit("setActivedPage", "/about");
-    document.title = "About | NGC";
-    //Socket.io
-    this.context = this.$refs.board.getContext("2d");
-    this.context = this.$refs.board.getContext("2d");
-    this.socket.on("position", data => {
-      this.position = data;
+      this.position.x = this.position.originX;
+      this.position.y = this.position.originY;
+      this.doDraw();
+    },
+    collidingDetect() {
+      let trigger = false;
+      this.builds.forEach(element => {
+        if (this.rectsColliding(this.position, element)) {
+          this.dialog = element.infos;
+          trigger = true;
+        }
+      });
+      if (trigger == false)
+        this.dialog =
+          "????????????????????????????????????????????????????????????????????????";
+    },
+    doDraw() {
+      this.context = this.$refs.board.getContext("2d");
       this.context.clearRect(
         0,
         0,
@@ -78,21 +109,31 @@ export default {
         this.$refs.board.width,
         this.$refs.board.width
       );
-      this.context.fillStyle = "#e53935";
-      this.context.fillRect(0, 100, 50, 1000);
-      this.context.fillStyle = "#3949ab";
-      this.context.fillRect(70, 0, 70, 70);
-      this.context.fillStyle = "#3949ab";
-      this.context.fillRect(350, 0, 700, 70);
-      this.context.fillStyle = "#e53935";
-      this.context.fillRect(160, 200, 50, 700);
-      this.context.fillStyle = "#e53935";
-      this.context.fillRect(350, 200, 100, 700);
-      this.context.fillStyle = "#e53935";
-      this.context.fillRect(600, 200, 50, 700);
+      this.builds.forEach(element => {
+        this.context.fillStyle = "#3949ab";
+        this.context.fillRect(element.x, element.y, element.w, element.h);
+      });
       this.context.fillStyle = "#ffb300";
-      this.context.fillRect(this.position.x, this.position.y, 20, 20);
-    });
+      this.context.fillRect(
+        this.position.x,
+        this.position.y,
+        this.position.w,
+        this.position.h
+      );
+    },
+    rectsColliding(rect1, rect2) {
+      return (
+        rect1.x < rect2.x + rect2.w &&
+        rect1.x + rect1.w > rect2.x &&
+        rect1.y < rect2.y + rect2.h &&
+        rect1.y + rect1.h > rect2.y
+      );
+    }
+  },
+  mounted() {
+    this.$store.commit("setActivedPage", "/about");
+    document.title = "About | NGC";
+    this.doDraw();
   },
   computed: {}
 };
